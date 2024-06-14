ec1ae9dc306d8b644faedb47dcd12dd0230dc3da
Jan Kubisa <s27920@pjwstk.edu.pl>
2024-06-14 10:07:13 +0200

    not enough time

--- WebApplication1/Controllers/ShopController.cs
+++ WebApplication1/Controllers/ShopController.cs
@@ -16,7 +16,7 @@ public class ShopController : ControllerBase
         return Ok(await Service.getClientByIdASyc(id));
     }
     [HttpPost("subscriptions")]
-    public async Task<int> InsertSubscriptionAsync([FromBody] SubscriptionRequestDto dto, [FromRoute]double amount)
+    public async Task<IActionResult> InsertSubscriptionAsync([FromBody] SubscriptionRequestDto dto, [FromRoute]double amount)
     {
         return Ok(await Service.InsertSubscriptionAsync(dto, amount));
     }
